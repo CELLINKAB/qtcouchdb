b4dd150a904ce31d213b16ec08520e4daab4e2e7
J-P Nurmi <jpnurmi@gmail.com>
2020-09-16 00:07:17 +0200

    Add "example"

---
+++ examples/couchview/couchview.cpp
@@ -0,0 +1,47 @@
+#include <QtCouchDB>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication app(argc, argv);
+
+    CouchDB db;
+    db.setServerConfiguration("localhost", 5984, "admin", "password");
+
+    // \0/
+    QObject::connect(&db, &CouchDB::installationChecked, [&](const CouchDBResponse& response) {
+        qDebug() << "-> installationChecked" << response.document();
+        QObject::connect(&db, &CouchDB::sessionStarted, [&](const CouchDBResponse& response) {
+            qDebug() << "-> sessionStarted" << response.document();
+            QObject::connect(&db, &CouchDB::databaseCreated, [&](const CouchDBResponse& response) {
+                qDebug() << "-> databaseCreated" << response.document();
+                QObject::connect(&db, &CouchDB::databasesListed, [&](const CouchDBResponse& response) {
+                    qDebug() << "-> databasesListed" << response.document();
+                    QObject::connect(&db, &CouchDB::databaseDeleted, [&](const CouchDBResponse& response) {
+                        qDebug() << "-> databaseDeleted" << response.document();
+                        QObject::connect(&db, &CouchDB::sessionEnded, [&](const CouchDBResponse& response) {
+                            qDebug() << "-> sessionEnded" << response.document();
+                            app.quit();
+                        });
+                        db.endSession();
+                    });
+                    db.deleteDatabase("qtcouchdb");
+                });
+                db.listDatabases();
+            });
+            db.createDatabase("qtcouchdb");
+        });
+        db.startSession("admin", "password");
+    });
+    db.checkInstallation();
+
+//    void documentsListed(const CouchDBResponse& response);
+//    void revisionRetrieved(const CouchDBResponse& response);
+//    void documentRetrieved(const CouchDBResponse& response);
+//    void documentUpdated(const CouchDBResponse& response);
+//    void documentDeleted(const CouchDBResponse& response);
+//    void attachmentUploaded(const CouchDBResponse& response);
+//    void attachmentDeleted(const CouchDBResponse& response);
+//    void databaseReplicated(const CouchDBResponse& response);
+
+    return app.exec();
+}
---
+++ examples/couchview/couchview.pro
@@ -0,0 +1,8 @@
+TEMPLATE = app
+QT += couchdb
+
+SOURCES += \
+    couchview.cpp
+
+target.path = $$[QT_INSTALL_EXAMPLES]/couchview
+INSTALLS += target
