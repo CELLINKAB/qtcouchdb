aa410b07e24ace2d1277c61a781d75d8e5a61253
J-P Nurmi <jpnurmi@gmail.com>
2020-09-16 22:59:07 +0200

    CouchClient: cosmetic cleanup

--- src/couchdb/couchclient.h
+++ src/couchdb/couchclient.h
@@ -26,6 +26,31 @@ public:
     Q_INVOKABLE QUrl documentUrl(const QString &databaseName, const QString &documentId, const QString &revision = QString()) const;
     Q_INVOKABLE QUrl attachmentUrl(const QString &databaseName, const QString &documentId, const QString &attachmentName, const QString &revision = QString()) const;
 
+public slots:
+    void checkInstallation();
+
+    void startSession(const QString &username, const QString &password);
+    void endSession();
+
+    void listDatabases();
+    void createDatabase(const QString &database);
+    void deleteDatabase(const QString &database);
+
+    void listDocuments(const QString &database);
+    void retrieveRevision(const QString &database, const QString &documentId);
+    void retrieveDocument(const QString &database, const QString &documentId);
+    void updateDocument(const QString &database, const QString &documentId, const QByteArray &document);
+    void deleteDocument(const QString &database, const QString &documentId, const QString &revision);
+
+    void uploadAttachment(const QString &database, const QString &documentId, const QString &attachmentName, const QByteArray &attachment,
+                          const QString &mimeType, const QString &revision);
+    void deleteAttachment(const QString &database, const QString &documentId, const QString &attachmentName, const QString &revision);
+
+    void replicateDatabaseFrom(const QUrl &sourceServer, const QString &sourceDatabase, const QString &targetDatabase,
+                               bool createTarget, bool continuous, bool cancel = false);
+    void replicateDatabaseTo(const QUrl &targetServer, const QString &sourceDatabase, const QString &targetDatabase,
+                             bool createTarget, bool continuous, bool cancel = false);
+
 signals:
     void installationChecked(const CouchResponse &response);
     void sessionStarted(const CouchResponse &response);
@@ -41,31 +66,6 @@ signals:
     void attachmentUploaded(const CouchResponse &response);
     void attachmentDeleted(const CouchResponse &response);
     void databaseReplicated(const CouchResponse &response);
-
-public slots:
-    Q_INVOKABLE void checkInstallation();
-
-    Q_INVOKABLE void startSession(const QString &username, const QString &password);
-    Q_INVOKABLE void endSession();
-
-    Q_INVOKABLE void listDatabases();
-    Q_INVOKABLE void createDatabase(const QString &database);
-    Q_INVOKABLE void deleteDatabase(const QString &database);
-
-    Q_INVOKABLE void listDocuments(const QString &database);
-    Q_INVOKABLE void retrieveRevision(const QString &database, const QString &documentId);
-    Q_INVOKABLE void retrieveDocument(const QString &database, const QString &documentId);
-    Q_INVOKABLE void updateDocument(const QString &database, const QString &documentId, const QByteArray &document);
-    Q_INVOKABLE void deleteDocument(const QString &database, const QString &documentId, const QString &revision);
-
-    Q_INVOKABLE void uploadAttachment(const QString &database, const QString &documentId, const QString &attachmentName, const QByteArray &attachment,
-                                      const QString &mimeType, const QString &revision);
-    Q_INVOKABLE void deleteAttachment(const QString &database, const QString &documentId, const QString &attachmentName, const QString &revision);
-
-    Q_INVOKABLE void replicateDatabaseFrom(const QUrl &sourceServer, const QString &sourceDatabase, const QString &targetDatabase,
-                                           bool createTarget, bool continuous, bool cancel = false);
-    Q_INVOKABLE void replicateDatabaseTo(const QUrl &targetServer, const QString &sourceDatabase, const QString &targetDatabase,
-                                         bool createTarget, bool continuous, bool cancel = false);
 
 protected:
     void executeQuery(const CouchQuery &query);
