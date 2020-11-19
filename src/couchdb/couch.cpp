d221b1ff6949e76f7b4e072398a68b40da86588e
J-P Nurmi <jpnurmi@gmail.com>
2020-11-19 11:25:50 +0100

    Restore Couch::foo() for creating requests etc.

--- src/couchdb/couch.cpp
+++ src/couchdb/couch.cpp
@@ -1,5 +1,69 @@
 #include "couch.h"
+#include "couchurl_p.h"
 
 Couch::Couch(QObject *parent) : QObject(parent)
 {
+}
+
+QUrl Couch::databaseUrl(const QUrl &baseUrl, const QString &name)
+{
+    return CouchUrl::resolve(baseUrl, name);
+}
+
+CouchRequest Couch::listAllDatabases(const QUrl &baseUrl)
+{
+    CouchRequest request(CouchRequest::Get);
+    request.setUrl(CouchUrl::resolve(baseUrl, QStringLiteral("_all_dbs")));
+    return request;
+}
+
+CouchRequest Couch::createDatabase(const QUrl &databaseUrl)
+{
+    CouchRequest request(CouchRequest::Put);
+    request.setUrl(databaseUrl);
+    return request;
+}
+
+CouchRequest Couch::deleteDatabase(const QUrl &databaseUrl)
+{
+    CouchRequest request(CouchRequest::Delete);
+    request.setUrl(databaseUrl);
+    return request;
+}
+
+CouchRequest Couch::listAllDocuments(const QUrl &databaseUrl)
+{
+    CouchRequest request(CouchRequest::Get);
+    request.setUrl(CouchUrl::resolve(databaseUrl, QStringLiteral("_all_docs")));
+    return request;
+}
+
+CouchRequest Couch::createDocument(const QUrl &databaseUrl, const QByteArray &content)
+{
+    CouchRequest request(CouchRequest::Post);
+    request.setUrl(databaseUrl);
+    request.setBody(content);
+    return request;
+}
+
+CouchRequest Couch::getDocument(const QUrl &databaseUrl, const QString &documentId, const QString &revision)
+{
+    CouchRequest request(CouchRequest::Get);
+    request.setUrl(CouchUrl::resolve(databaseUrl, documentId, revision));
+    return request;
+}
+
+CouchRequest Couch::updateDocument(const QUrl &databaseUrl, const QString &documentId, const QString &revision, const QByteArray &content)
+{
+    CouchRequest request(CouchRequest::Put);
+    request.setUrl(CouchUrl::resolve(databaseUrl, documentId, revision));
+    request.setBody(content);
+    return request;
+}
+
+CouchRequest Couch::deleteDocument(const QUrl &databaseUrl, const QString &documentId, const QString &revision)
+{
+    CouchRequest request(CouchRequest::Delete);
+    request.setUrl(CouchUrl::resolve(databaseUrl, documentId, revision));
+    return request;
 }
--- src/couchdb/couch.h
+++ src/couchdb/couch.h
@@ -2,6 +2,7 @@
 #define COUCH_H
 
 #include <QtCouchDB/couchglobal.h>
+#include <QtCouchDB/couchrequest.h>
 #include <QtCore/qobject.h>
 
 class COUCHDB_EXPORT Couch : public QObject
@@ -10,6 +11,18 @@ class COUCHDB_EXPORT Couch : public QObject
 
 public:
     explicit Couch(QObject *parent = nullptr);
+
+    Q_INVOKABLE static QUrl databaseUrl(const QUrl &baseUrl, const QString &name);
+
+    Q_INVOKABLE static CouchRequest listAllDatabases(const QUrl &baseUrl);
+    Q_INVOKABLE static CouchRequest createDatabase(const QUrl &databaseUrl);
+    Q_INVOKABLE static CouchRequest deleteDatabase(const QUrl &databaseUrl);
+
+    Q_INVOKABLE static CouchRequest listAllDocuments(const QUrl &databaseUrl);
+    Q_INVOKABLE static CouchRequest createDocument(const QUrl &databaseUrl, const QByteArray &content);
+    Q_INVOKABLE static CouchRequest getDocument(const QUrl &databaseUrl, const QString &documentId, const QString &revision = QString());
+    Q_INVOKABLE static CouchRequest updateDocument(const QUrl &databaseUrl, const QString &documentId, const QString &revision, const QByteArray &content);
+    Q_INVOKABLE static CouchRequest deleteDocument(const QUrl &databaseUrl, const QString &documentId, const QString &revision);
 };
 
 #endif // COUCH_H
