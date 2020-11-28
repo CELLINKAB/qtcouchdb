b3252a201be29dcbd296217bc76cab58ed0200e0
J-P Nurmi <jpnurmi@gmail.com>
2020-11-28 10:28:55 +0100

    Add QML type registrations

--- src/imports/couchdb/couchdbplugin.cpp
+++ src/imports/couchdb/couchdbplugin.cpp
@@ -1,5 +1,13 @@
 #include <QtCouchDB/couch.h>
 #include <QtCouchDB/couchclient.h>
+#include <QtCouchDB/couchdatabase.h>
+#include <QtCouchDB/couchdesigndocument.h>
+#include <QtCouchDB/couchdocument.h>
+#include <QtCouchDB/coucherror.h>
+#include <QtCouchDB/couchquery.h>
+#include <QtCouchDB/couchrequest.h>
+#include <QtCouchDB/couchresponse.h>
+#include <QtCouchDB/couchview.h>
 #include <QtQml/qqml.h>
 #include <QtQml/qqmlengine.h>
 #include <QtQml/qqmlextensionplugin.h>
@@ -15,10 +23,19 @@ public:
 
 void CouchDBPlugin::registerTypes(const char *uri)
 {
+    qRegisterMetaType<CouchDocument>();
+    qRegisterMetaType<CouchError>();
+    qRegisterMetaType<CouchQuery>();
+    qRegisterMetaType<CouchRequest>();
+
     qmlRegisterSingletonType<Couch>(uri, 1, 0, "Couch", [](QQmlEngine *engine, QJSEngine *) -> QObject * {
         return new Couch(engine);
     });
     qmlRegisterType<CouchClient>(uri, 1, 0, "CouchClient");
+    qmlRegisterType<CouchDatabase>(uri, 1, 0, "CouchDatabase");
+    qmlRegisterType<CouchDesignDocument>(uri, 1, 0, "CouchDesignDocument");
+    qmlRegisterUncreatableType<CouchResponse>(uri, 1, 0, "CouchResponse", tr("Use CouchClient.sendRequest()"));
+    qmlRegisterType<CouchView>(uri, 1, 0, "CouchView");
 }
 
 #include "couchdbplugin.moc"
